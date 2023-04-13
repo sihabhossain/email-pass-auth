fe7f3fe429b6e46ed8504ca1598e3218f613586d
Sihab Hossain <sihabhossain380@gmail.com>
2023-04-13 22:51:36 +0600

    email password implemented

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Register = () => {
+  const [email, setEmail] = useState("");
+
+  const handleEmailChange = (event) => {
+    console.log(event.target.value);
+  };
+
+  const handlePasswordBlur = (event) => {
+    console.log(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+    console.log(email, password);
+  };
+
   return (
     <div>
       <h2>Register</h2>
 
-      <form>
-        <input type="email" name="email" id="email" />
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={handleEmailChange}
+          type="email"
+          name="email"
+          id="email"
+        />
         <br />
-        <input type="password" name="password" id="password" />
+        <input
+          onBlur={handlePasswordBlur}
+          type="password"
+          name="password"
+          id="password"
+        />
         <br />
         <input type="submit" value="Register" />
       </form>
