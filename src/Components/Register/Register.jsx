9cb0138d5b5da4c0eeb88bf0a663162357335c2a
Sihab Hossain <sihabhossain380@gmail.com>
2023-04-13 22:23:02 +0600

    register form added

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -4,6 +4,14 @@ const Register = () => {
   return (
     <div>
       <h2>Register</h2>
+
+      <form>
+        <input type="email" name="email" id="email" />
+        <br />
+        <input type="password" name="password" id="password" />
+        <br />
+        <input type="submit" value="Register" />
+      </form>
     </div>
   );
 };
