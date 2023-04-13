08bed8ac59a0cf904af9927f5a9af19de0435fb1
Sihab Hossain <sihabhossain380@gmail.com>
2023-04-13 23:18:17 +0600

    react bs and bs added

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Main from "./Components/Layout/Main";
 import Login from "./Components/Login/Login";
 import Register from "./Components/Register/Register";
 import Home from "./Components/Home/Home";
+import RegisterRBS from "./Components/RegisterRBS/RegisterRBS";
+import RegisterBS from "./Components/RegisterBS/RegisterBS";
 
 const router = createBrowserRouter([
   {
@@ -24,6 +26,14 @@ const router = createBrowserRouter([
       {
         path: "register",
         element: <Register></Register>,
+      },
+      {
+        path: "register-rbs",
+        element: <RegisterRBS></RegisterRBS>,
+      },
+      {
+        path: "register-bs",
+        element: <RegisterBS></RegisterBS>,
       },
     ],
   },
